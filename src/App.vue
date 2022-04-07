d71b06b40dc5a49dbaa5890d1902ba06761a1992
Lepist <rootfix25@gmail.com>
2022-04-07 22:16:06 +0300

    added directive v-bind and v-on

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="App">
+    <h4>Название поста</h4>
+    <input v-bind:value="title" type="text" placeholder="Название">
+    <input v-bind:value="body" @input="input" type="text" placeholder="Описание">
+    <button class="btn">Создать</button>
+    <div class="post" v-for="post in posts">
+      <p><strong>Название: </strong> {{ post.title }}</p>
+      <p><strong>Описание: </strong> {{ post.body }}</p>
+    </div>
+  </div>
 </template>
 
+<script>
+export default {
+  data(){
+    return {
+      posts: [
+        {id: 1, title: "Название JS", body: "Описание 1"},
+        {id: 2, title: "Название JS 2", body: "Описание 2"},
+        {id: 3, title: "Название JS 3", body: "Описание 3"},
+        {id: 4, title: "Название JS 4", body: "Описание 4"}
+
+      ],
+      title: '',
+      body: ''
+    }
+  }
+} 
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
